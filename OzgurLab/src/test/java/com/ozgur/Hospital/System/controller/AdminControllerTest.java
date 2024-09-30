75b02d77cfa419ba2bac3f9a3d73c8e8bca30db1
GorkemBedel <117198668+GorkemBedel@users.noreply.github.com>
2024-09-30 23:36:29 +0300

    Readme File is updated

---
+++ OzgurLab/src/test/java/com/ozgur/Hospital/System/controller/AdminControllerTest.java
@@ -0,0 +1,29 @@
+package com.ozgur.Hospital.System.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(AdminController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
+public class AdminControllerTest {
+
+    @BeforeEach
+    void setUp() {
+        
+    }
+
+    @Test
+    void name() {
+    }
+
+    @AfterEach
+    void tearDown() {
+        
+    }
+}
